dc6b781730d0c44b48a0ac0b26debfbf289a5587
Konrad <konrad.domalewski@gmail.com>
2023-05-27 00:25:40 +0200

    back to inital

--- index.js
+++ index.js
@@ -3,18 +3,10 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
-app.use(express.json());
-
-const dataTrains = require('./data/trains.json');
-
 app.get('/', (req, res) => {
-    res.send('Hello from Nerdbord!');
-});
-
-app.get('/trains', (req, res) => {
-    res.status(200).send(JSON.stringify(dataTrains));
+    res.send('Hello from Nerdbord!')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+    console.log('Server listening on port 3000')
 })
