8cae798922d0122eb5b1134338ad06e11fb88b1c
Konrad <konrad.domalewski@gmail.com>
2023-05-29 14:56:05 +0200

    addint PUT

--- index.js
+++ index.js
@@ -24,3 +24,56 @@ app.post("/trains", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+app.put("/trains/:id", (req, res) => {
+  const newData = req.body;
+
+  const filePath = path.join(__dirname, "data", "trains.json");
+
+  let id;
+  const idParams = req.params.id;
+  const idBody = req.body.id;
+
+  if (idParams !== idBody) {
+    res.status(500).send("Ids in params and body do not match");
+    return;
+  } else {
+    id = idParams;
+  }
+
+  if (!id) {
+    res.status(500).send("No id provided");
+    return;
+  }
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      // handle error
+      console.error(err);
+      res.status(500).send("Error reading file");
+      return;
+    }
+
+    let trains = JSON.parse(data);
+
+    let trainToModify = trains.find((train) => train.id === id);
+
+    if (!trainToModify) {
+      res.status(500).send(`No train with id ${id} found`);
+      return;
+    } else {
+      for (let property in newData) {
+        trainToModify[property] = newData[property];
+      }
+    }
+    fs.writeFile(filePath, JSON.stringify(trains, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error writing file");
+        return;
+      }
+
+      res.status(200).send(`Successfully modified train with id ${id}`);
+    });
+  });
+});
